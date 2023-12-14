eadeb1321c07c11cd751c899f27701dc10806a1e
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2023-12-14 15:46:21 -0600

    added info to readme and an images folder

--- Assets/script.js
+++ Assets/script.js
@@ -6,65 +6,71 @@
 var today = dayjs();
 $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
 
-
 // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+$("#9").children("button").on("click", function () {
+  localStorage.setItem("event9am", $("#9").children("textarea").val());
+  alert("Appointment added to localStorage!");
+});
 
-  $("#9").children("button").on("click", function() {
-    localStorage.setItem("event9am", $("#9").children("textarea").val())
-  });
+$("#10").children("button").on("click", function () {
+  localStorage.setItem("event10am", $("#10").children("textarea").val());
+  alert("Appointment added to localStorage!");
+});
 
-  $("#10").children("button").on("click", function() {
-    localStorage.setItem("event10am", $("#10").children("textarea").val())
-  });
+$("#11").children("button").on("click", function () {
+  localStorage.setItem("event11am", $("#11").children("textarea").val());
+  alert("Appointment added to localStorage!");
+});
 
-  $("#11").children("button").on("click", function() {
-    localStorage.setItem("event11am", $("#11").children("textarea").val())
-  });
+$("#12").children("button").on("click", function () {
+  localStorage.setItem("event12pm", $("#12").children("textarea").val());
+  alert("Appointment added to localStorage!");
+});
 
-  $("#12").children("button").on("click", function() {
-    localStorage.setItem("event12pm", $("#12").children("textarea").val())
-  });
+$("#13").children("button").on("click", function () {
+  localStorage.setItem("event1pm", $("#13").children("textarea").val());
+  alert("Appointment added to localStorage!");
+});
 
-  $("#13").children("button").on("click", function() {
-    localStorage.setItem("event1pm", $("#13").children("textarea").val())
-  });
-  
-  $("#14").children("button").on("click", function() {
-    localStorage.setItem("event2pm", $("#14").children("textarea").val())
-  });
+$("#14").children("button").on("click", function () {
+  localStorage.setItem("event2pm", $("#14").children("textarea").val());
+  alert("Appointment added to localStorage!");
+});
 
-  $("#15").children("button").on("click", function() {
-    localStorage.setItem("event3pm", $("#15").children("textarea").val())
-  });
+$("#15").children("button").on("click", function () {
+  localStorage.setItem("event3pm", $("#15").children("textarea").val());
+  alert("Appointment added to localStorage!");
+});
 
-  $("#16").children("button").on("click", function() {
-    localStorage.setItem("event4pm", $("#16").children("textarea").val())
-  });
+$("#16").children("button").on("click", function () {
+  localStorage.setItem("event4pm", $("#16").children("textarea").val());
+  alert("Appointment added to localStorage!");
+});
 
-  $("#17").children("button").on("click", function() {
-    localStorage.setItem("event5pm", $("#17").children("textarea").val())
-  });
-  
-  $(function () {
+$("#17").children("button").on("click", function () {
+  localStorage.setItem("event5pm", $("#17").children("textarea").val());
+  alert("Appointment added to localStorage!");
+});
+
+$(function () {
   //   TODO: Add code to get any user input that was saved in localStorage and set
   //   the values of the corresponding textarea elements. HINT: How can the id
   //   attribute of each time-block be used to do this?
-    $("#9").children("textarea").val(localStorage.getItem("event9am"));
-    $("#10").children("textarea").val(localStorage.getItem("event10am"));
-    $("#11").children("textarea").val(localStorage.getItem("event11am"));
-    $("#12").children("textarea").val(localStorage.getItem("event12pm"));
-    $("#13").children("textarea").val(localStorage.getItem("event1pm"));
-    $("#14").children("textarea").val(localStorage.getItem("event2pm"));
-    $("#15").children("textarea").val(localStorage.getItem("event3pm"));
-    $("#16").children("textarea").val(localStorage.getItem("event4pm"));
-    $("#17").children("textarea").val(localStorage.getItem("event5pm"));
-    });
-
+  $("#9").children("textarea").val(localStorage.getItem("event9am"));
+  $("#10").children("textarea").val(localStorage.getItem("event10am"));
+  $("#11").children("textarea").val(localStorage.getItem("event11am"));
+  $("#12").children("textarea").val(localStorage.getItem("event12pm"));
+  $("#13").children("textarea").val(localStorage.getItem("event1pm"));
+  $("#14").children("textarea").val(localStorage.getItem("event2pm"));
+  $("#15").children("textarea").val(localStorage.getItem("event3pm"));
+  $("#16").children("textarea").val(localStorage.getItem("event4pm"));
+  $("#17").children("textarea").val(localStorage.getItem("event5pm"));
+});
 
 // Gets the current hour in 24-hour time
 var currentHour = dayjs().hour();
@@ -91,6 +97,3 @@ $(function () {
       }
   });
 });
-
-
-
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Work-Day-Scheduler
+
+ 
+
+## Description
+
+This is a website designed to help the user organize their day by scheduling events into hour time blocks.
+
+## Installation
+
+N/A
+
+## Usage
+
+The user can input information into each time block. When the user clicks the save button, the information gets saved into localStorage and will persist even if the page is refreshed. The time blocks are color coded by past hours (gray), present hour (red), and future hours (green). The current date will always be displayed at the top of the page.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
+
+## Screenshot of Website
+
+![Alt text](./Assets/images/Screenshot%202023-12-14%20154450.png)
